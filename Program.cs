e4c0b3b19528c681452a36e6b9cb68e3d0973293
NSPatton <greengun15@gmail.com>
2021-05-21 15:44:11 -0500

    The moose asks questions

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    bool MooseAsks(string question)
+    {
+        Console.Write($"{question} (Y/N): ");
+        string answer = Console.ReadLine().ToLower();
+
+        while (answer != "y" && answer != "n")
+        {
+            Console.Write($"{question} (Y/N): ");
+            answer = Console.ReadLine().ToLower();
+        }
+
+        if (answer == "y")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    void MooseSays(string message)
+    {
+        Console.WriteLine($@"
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+                       ");
+    }
+
+    Question("is Canada real?", "Really? It seems very unlikely", "I  K N E W  I T !!!");
+    Question("Are you enthusiastic?", "Yay!", "You should try it!");
+    Question("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...");
+    Question("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
+
+    void Question(string question, string trueanswer, string falseanswer)
+    {
+        bool isTrue = MooseAsks(question);
+        if (isTrue)
+        {
+            MooseSays(trueanswer);
+        }
+        else
+        {
+            MooseSays(falseanswer);
+        }
+    }
+}
+
